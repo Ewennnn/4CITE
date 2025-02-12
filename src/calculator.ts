18c07eb6cb13c18e3dfa47a47180d63cb0f16fe3
Ewennn <ewen.bosquet@gmail.com>
2025-02-12 11:09:11 +0100

    refract/ Remove useless condition in divide function

--- src/calculator.ts
+++ src/calculator.ts
@@ -6,10 +6,6 @@ export function divide(a: number, b: number): number {
     if(b === 0) {
         throw new Error("Divide by 0");
     }
-    
-    if (b === 1) {
-        return a
-    }
 
     return a / b
 }
